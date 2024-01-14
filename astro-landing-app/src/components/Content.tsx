7d50f29debd4df11e6d3dd44d844e087c37d2297
teradonburi <daikiterai@gmail.com>
2024-01-14 18:05:28 +0900

    comment

--- astro-landing-app/src/components/Content.tsx
+++ astro-landing-app/src/components/Content.tsx
@@ -8,6 +8,7 @@ const Content: React.FC<Props> = (props) => {
   const { num } = props
   return (
     <>
+      {/* create doms */}
       {[...new Array(100).keys()].map((_, i) => <div key={num * 100 + i}>Hello World{num * 100 + i}</div>)}
     </>
   )
